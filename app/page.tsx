1c7a140d205be2c97df70fe70b9edea086d5d405
Perrin <perrin.grandne@gmail.com>
2026-03-16 11:13:00 +0100

    Add footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { ChartAreaInteractive } from "@/components/ChartAreaInteractive";
 import { SearchInput } from "@/components/SearchInput";
 import { Transaction } from "@/types/interface";
+import { GithubIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function Page() {
@@ -21,6 +22,27 @@ export default function Page() {
           userDeposit={userDeposit}
         />
       </div>
+      <footer className="flex items-center justify-center gap-4 text-sm text-muted-foreground">
+        <a
+          href="https://github.com/pgrandne/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-1 hover:text-foreground transition-colors"
+        >
+          <GithubIcon className="h-4 w-4" />
+          GitHub
+        </a>
+        <span>·</span>
+
+        <a
+          href="https://perrin.website/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-foreground transition-colors"
+        >
+          My site
+        </a>
+      </footer>
     </div>
   );
 }
